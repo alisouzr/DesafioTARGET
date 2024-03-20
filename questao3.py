3276d285f153aa4d4217789b4226a1127add9620
Aliny Souza <79667413+alisouzr@users.noreply.github.com>
2024-03-20 17:34:32 +0000

    Adiciona

---
+++ questao3.py
@@ -0,0 +1,22 @@
+'''3) Descubra a lógica e complete o próximo elemento:
+
+a) 1, 3, 5, 7, 9
+- números ímpares
+
+b) 2, 4, 8, 16, 32, 64, 128
+- próxima sequência o dobro do anterior
+
+c) 0, 1, 4, 9, 16, 25, 36, 49
+- próxima sequência do quadrado do anterior (0^2 =0; 1^2 = 1; 2^2 = 4; 3^2 = 9; 4^2 = 16....7^2 = 49)
+
+d) 4, 16, 36, 64, 100
+- próximo será 100 ( 16 - 4 = 12; 36-16 = 20, 64 - 36 = 28)
+- 12, 20, 28 ... a cada número aumenta 8 unidades para o próximo, logo 28+8=36, então irá aumentar 36 unidades para o próximo número, 64+36 = 100
+
+e) 1, 1, 2, 3, 5, 8, 13
+- próximo é a soma dos dois anteriores, logo 1+1=2, 1+2=3, 2+3=5... 8+5=13
+
+f) 2,10, 12, 16, 17, 18, 19, 200
+- números que começam com a letra 'D'
+'''
+
---
+++ questao4.py
@@ -0,0 +1,10 @@
+'''Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?'''
+
+# 1 - Ligo duas lâmpadas e espero um pouco
+# 2 - Despois desligo uma lâmpada
+# 3 - Vou até a Sala
+# 4 - A lâmpada que estiver acesa é a que deixei ligada
+# 5 - A lâmpada que estiver quente é a lâmpada que desliguei
+# 6 - A outra é a que não liguei
